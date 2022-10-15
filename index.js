4b823605aabf81ec024404c2d2de4d5af0385f98
Marine-Dlb <mrn.bgmd@gmail.com>
2022-10-15 11:58:04 +0200

    input darkmode

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-let navbar = document.querySelector("navbar");
-navbar.addEventListener("click", (e) => {
-  
-  e.target.classList.toggle("");
-  
-});
+const checkbox = document.querySelector("#customSwitch1")
+const body = documetn.querySelector("body")
+
+const titres = document.querySelectorAll("h3")
+const iframes = document.querySelectorAll("iframe")
+const hrs = document.querySelectorAll("hr")
+const checkboxLabel = document.querySelector(".custom-control-input")
+const navbar = document.querySelectorAll("nav")
+const logoText = document.querySelectorAll("a")
+
+checkbox.addEventListener('clcik', function(){
+  body.classList.toggle("darkmode")
+})
