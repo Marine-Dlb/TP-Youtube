59d656cc0418523f84036f3658cd412f0454bae3
Marine-Dlb <mrn.bgmd@gmail.com>
2022-10-22 11:18:05 +0200

    darkmode

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const checkbox = document.querySelector("#customSwitch1")
-const body = documetn.querySelector("body")
+const checkbox = document.querySelector("#customSwitch1");
+const body = document.querySelector("body");
 
-const titres = document.querySelectorAll("h3")
-const iframes = document.querySelectorAll("iframe")
-const hrs = document.querySelectorAll("hr")
-const checkboxLabel = document.querySelector(".custom-control-input")
-const navbar = document.querySelectorAll("nav")
-const logoText = document.querySelectorAll("a")
+const titres = document.querySelectorAll("h3");
+const iframes = document.querySelectorAll("iframe");
+const hrs = document.querySelectorAll("hr");
+const checkboxLabel = document.querySelector(".custom-control-input");
+const navbar = document.querySelector("nav");
+const logoText = document.querySelector("a");
 
-checkbox.addEventListener('clcik', function(){
-  body.classList.toggle("darkmode")
+checkbox.addEventListener('click', function(){
+  body.classList.toggle("darkmode");
+  checkboxLabel.classList.toggle("darkmode");
+  navbar.classList.toggle("darkmode");
+  logoText.classList.toggle("darkmode");
+  titres.forEach(titre=>{
+    titre.classList.toggle("darkmode");
 })
+hrs.forEach(hr=>{
+  hr.classList.toggle("darkmode");
+})
+});
